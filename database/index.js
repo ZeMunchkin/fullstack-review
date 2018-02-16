9fcc30195847f05c2654ced041106abb6f4d14af
Sara Brandes <smbrandes17@gmail.com>
2018-02-16 13:11:56 -0800

    have schema designed

--- database/index.js
+++ database/index.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+// new is between comments
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('We\'re connected!');
+})
+// end
+
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+
+  gitHubId: Number,
+  repoName: String,
+  ownerName: String,
+  ownerId: Number,
+  url: String,
+  forks: Number,
+  watchers: Number
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
+
 
 let save = (/* TODO */) => {
   // TODO: Your code here
